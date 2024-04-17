010bc1b08c6333b43dfe0456b83f21c39ed666c1
rebeccahongsf <rebeccahongsf@gmail.com>
2024-04-16 17:17:12 -0700

    SUM-46 | @rebeccahongsf | theme alert message

--- src/components/config-pages/global-message.tsx
+++ src/components/config-pages/global-message.tsx
@@ -1,17 +1,15 @@
-import {BellIcon, CheckCircleIcon, ExclamationTriangleIcon, InformationCircleIcon} from "@heroicons/react/20/solid";
-import {H2} from "@components/elements/headers";
+import { H2 } from "@components/elements/headers";
 import Wysiwyg from "@components/elements/wysiwyg";
 import Link from "@components/elements/link";
-import {clsx} from "clsx";
-import {StanfordGlobalMessage} from "@lib/gql/__generated__/drupal.d";
+import { clsx } from "clsx";
+import { StanfordGlobalMessage } from "@lib/gql/__generated__/drupal.d";
 
 const GlobalMessage = ({
   suGlobalMsgEnabled,
   suGlobalMsgType,
-  suGlobalMsgLabel,
   suGlobalMsgHeader,
   suGlobalMsgLink,
-  suGlobalMsgMessage
+  suGlobalMsgMessage,
 }: StanfordGlobalMessage) => {
   if (!suGlobalMsgEnabled) return;
 
@@ -24,38 +22,30 @@ const GlobalMessage = ({
   });
 
   return (
-    <div className={wrapperClasses + " py-10"}>
-      <div className="centered flex flex-col lg:flex-row gap-10">
-        <div className="flex items-center leading-none shrink-0">
-          <MessageIcon messageType={suGlobalMsgType}/>
-          {suGlobalMsgLabel}:
-        </div>
-        <div className="[&_a]:text-white [&_a.btn]:bg-transparent [&_a.btn]:border-2 [&_a.btn]:border-white">
-          {suGlobalMsgHeader && <H2>{suGlobalMsgHeader}</H2>}
+    <div className='bg-fog-light'>
+      <div className='centered'>
+        <div
+          className={
+            wrapperClasses +
+            " px-16 py-10 rounded w-fit flex flex-col items-center md:flex-row gap-10"
+          }
+        >
+          <div>
+            {suGlobalMsgHeader && (
+              <H2 className='text-23 mb-3'>{suGlobalMsgHeader}</H2>
+            )}
+            <Wysiwyg html={suGlobalMsgMessage?.processed} />
+          </div>
 
-          <Wysiwyg html={suGlobalMsgMessage?.processed}/>
-
-          {suGlobalMsgLink?.url &&
-            <Link href={suGlobalMsgLink.url} className="text-white">
+          {suGlobalMsgLink?.url && (
+            <Link href={suGlobalMsgLink.url} className='link--action'>
               {suGlobalMsgLink.title}
             </Link>
-          }
+          )}
         </div>
       </div>
     </div>
-  )
-}
-
-const MessageIcon = ({messageType}: { messageType: StanfordGlobalMessage["suGlobalMsgType"] }) => {
-  switch (messageType) {
-    case "info":
-      return <InformationCircleIcon width={40}/>
-    case "success":
-      return <CheckCircleIcon width={40}/>
-    case "plain":
-      return <BellIcon width={40}/>;
-  }
-  return <ExclamationTriangleIcon width={40}/>;
-}
+  );
+};
 
 export default GlobalMessage;

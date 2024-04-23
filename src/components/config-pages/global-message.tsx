0d113c3fd07288fc72bf3cb5b94787986fad2dd6
rebeccahongsf <rebeccahongsf@gmail.com>
2024-04-23 10:01:34 -0700

    fixup quotes and spacing

--- src/components/config-pages/global-message.tsx
+++ src/components/config-pages/global-message.tsx
@@ -1,13 +1,21 @@
+import {
+  BellIcon,
+  CheckCircleIcon,
+  ExclamationTriangleIcon,
+  InformationCircleIcon,
+} from "@heroicons/react/20/solid";
 import { H2 } from "@components/elements/headers";
 import Wysiwyg from "@components/elements/wysiwyg";
-import { clsx } from "clsx";
 import { StanfordGlobalMessage } from "@lib/gql/__generated__/drupal.d";
 import ActionLink from "@components/elements/action-link";
+import { twMerge } from "tailwind-merge";
+import clsx from "clsx/clsx";
 
 const GlobalMessage = ({
   suGlobalMsgEnabled,
   suGlobalMsgType,
   suGlobalMsgHeader,
+  suGlobalMsgLabel,
   suGlobalMsgLink,
   suGlobalMsgMessage,
 }: StanfordGlobalMessage) => {
@@ -22,13 +30,13 @@ const GlobalMessage = ({
   });
 
   return (
-    <div className='bg-fog-light'>
-      <div className='md:centered'>
+    <div className="bg-fog-light">
+      <div className="md:centered">
         <div
-          className={
-            wrapperClasses +
-            " px-16 py-10 rounded-b-[25px] w-full lg:w-3/4 flex flex-col md:flex-row items-center gap-10"
-          }
+          className={twMerge(
+            wrapperClasses,
+            "px-16 py-10 rounded-b-[25px] w-full lg:w-3/4 flex flex-col md:flex-row items-center gap-10"
+          )}
         >
           {suGlobalMsgLabel && (
             <div className="flex items-center leading-none shrink-0">
@@ -37,9 +45,7 @@ const GlobalMessage = ({
             </div>
           )}
           <div>
-            {suGlobalMsgHeader && (
-              <H2 className='text-23 mb-3'>{suGlobalMsgHeader}</H2>
-            )}
+            {suGlobalMsgHeader && <H2 className="text-23 mb-3">{suGlobalMsgHeader}</H2>}
             <Wysiwyg html={suGlobalMsgMessage?.processed} />
           </div>
 
@@ -52,6 +58,22 @@ const GlobalMessage = ({
       </div>
     </div>
   );
+};
+
+const MessageIcon = ({
+  messageType,
+}: {
+  messageType: StanfordGlobalMessage["suGlobalMsgType"];
+}) => {
+  switch (messageType) {
+    case "info":
+      return <InformationCircleIcon width={40} />;
+    case "success":
+      return <CheckCircleIcon width={40} />;
+    case "plain":
+      return <BellIcon width={40} />;
+  }
+  return <ExclamationTriangleIcon width={40} />;
 };
 
 export default GlobalMessage;

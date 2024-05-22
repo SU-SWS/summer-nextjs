ec355f8d49bb351e25926ff2c3d5575de1190be4
rebeccahongsf <rebeccahongsf@gmail.com>
2024-05-21 17:24:42 -0700

    add play icon and transition

--- src/components/paragraphs/sum-video/video-paragraph.tsx
+++ src/components/paragraphs/sum-video/video-paragraph.tsx
@@ -3,6 +3,7 @@ import { HtmlHTMLAttributes, useState } from "react";
 import Oembed from "@components/elements/ombed";
 import clsx from "clsx";
 import { ParagraphSumVideo } from "@lib/gql/__generated__/drupal";
+import { PlayIcon } from "@heroicons/react/24/solid";
 
 type Props = HtmlHTMLAttributes<HTMLDivElement> & {
   paragraph: ParagraphSumVideo;
@@ -18,8 +19,9 @@ const SumVideoParagraph = ({ paragraph, ...props }: Props) => {
   };
 
   return (
-    <div onClick={handlePlay} className={clsx("relative overflow-hidden w-full h-[700px] cursor-pointer border-3 outline-3", { "rounded-none": isPlaying, "rounded-full": !isPlaying })}  {...props}>
-      {videoUrl && <Oembed url={videoUrl} className="absolute top-0 left-0 w-full h-full"/>}
+    <div  {...props} onClick={handlePlay} className={clsx("relative z-0 overflow-hidden w-full h-[700px] cursor-pointer border-4 border-white", { "rounded-none": isPlaying, "rounded-full": !isPlaying })}>
+      {!isPlaying && <div className="w-full h-full absolute z-50 flex items-center justify-center"><PlayIcon width={95} className="text-white bg-digital-red rounded-full p-10" /></div>}
+      {videoUrl && <Oembed url={videoUrl} className="z-10 absolute top-0 left-0 w-full h-full"/>}
     </div>
   )
 }

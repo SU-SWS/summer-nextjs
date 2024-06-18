5d6ff5d2acf48595e0b2aa6bf809d72817f7058d
pookmish <pookmish@gmail.com>
2024-06-18 14:33:54 -0700

    Update layout.tsx metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ const icons: Icon[] = [16, 32, 96, 128, 192, 196].map(size => ({
  * Metadata that does not change often.
  */
 export const metadata = {
-  metadataBase: new URL("https://somesite.stanford.edu"),
+  metadataBase: new URL("https://summer.stanford.edu"),
   title: "Stanford University",
   openGraph: {
     type: "website",

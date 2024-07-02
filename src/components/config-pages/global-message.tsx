14d89f3533a67b37ad93b07293f6e6e753e28e39
Mike Decker <pookmish@gmail.com>
2024-07-02 12:13:29 -0700

    Fixed accessibility issues on course detail page and global banner

--- src/components/config-pages/global-message.tsx
+++ src/components/config-pages/global-message.tsx
@@ -1,25 +1,13 @@
-import {
-  BellIcon,
-  CheckCircleIcon,
-  ExclamationTriangleIcon,
-  InformationCircleIcon,
-} from "@heroicons/react/20/solid";
-import { H2 } from "@components/elements/headers";
-import Wysiwyg from "@components/elements/wysiwyg";
-import { StanfordGlobalMessage } from "@lib/gql/__generated__/drupal.d";
-import ActionLink from "@components/elements/action-link";
-import { twMerge } from "tailwind-merge";
-import clsx from "clsx";
+import {BellIcon, CheckCircleIcon, ExclamationTriangleIcon, InformationCircleIcon} from "@heroicons/react/20/solid"
+import {H2} from "@components/elements/headers"
+import Wysiwyg from "@components/elements/wysiwyg"
+import {StanfordGlobalMessage} from "@lib/gql/__generated__/drupal.d"
+import ActionLink from "@components/elements/action-link"
+import {twMerge} from "tailwind-merge"
+import clsx from "clsx"
 
-const GlobalMessage = ({
-  suGlobalMsgEnabled,
-  suGlobalMsgType,
-  suGlobalMsgHeader,
-  suGlobalMsgLabel,
-  suGlobalMsgLink,
-  suGlobalMsgMessage,
-}: StanfordGlobalMessage) => {
-  if (!suGlobalMsgEnabled) return;
+const GlobalMessage = ({suGlobalMsgEnabled, suGlobalMsgType, suGlobalMsgHeader, suGlobalMsgLabel, suGlobalMsgLink, suGlobalMsgMessage}: StanfordGlobalMessage) => {
+  if (!suGlobalMsgEnabled) return
 
   const wrapperClasses = clsx({
     "bg-digital-blue-dark text-white": suGlobalMsgType === "info",
@@ -27,53 +15,53 @@ const GlobalMessage = ({
     "bg-digital-green text-white": suGlobalMsgType === "success",
     "bg-foggy-light": suGlobalMsgType === "plain",
     "bg-digital-red text-white": suGlobalMsgType === "error",
-  });
+  })
 
   return (
     <div className="bg-fog-light">
       <div className="md:centered">
-        <div
-          className={twMerge(
-            wrapperClasses,
-            "px-16 py-10 rounded-b-[25px] w-full lg:w-3/4 flex flex-col md:flex-row items-center gap-10"
-          )}
-        >
-          {suGlobalMsgLabel && 
-            <div className="flex items-center leading-none shrink-0">
+        <div className={twMerge(wrapperClasses, "flex w-full flex-col items-center gap-10 rounded-b-[25px] px-16 py-10 md:flex-row lg:w-3/4")}>
+          {suGlobalMsgLabel && (
+            <div className="flex shrink-0 items-center leading-none">
               <MessageIcon messageType={suGlobalMsgType} />
               {suGlobalMsgLabel}:
             </div>
-          }
+          )}
           <div>
-            {suGlobalMsgHeader && <H2 className="text-23 mb-3">{suGlobalMsgHeader}</H2>}
+            {suGlobalMsgHeader && <H2 className="mb-3 text-23">{suGlobalMsgHeader}</H2>}
             <Wysiwyg html={suGlobalMsgMessage?.processed} />
           </div>
 
-          {suGlobalMsgLink?.url && 
-            <ActionLink href={suGlobalMsgLink.url} className="w-full max-w-fit">
+          {suGlobalMsgLink?.url && (
+            <ActionLink
+              href={suGlobalMsgLink.url}
+              className={twMerge(
+                "w-full max-w-fit no-underline hocus:underline",
+                clsx({
+                  "text-black": suGlobalMsgType === "warning",
+                  "text-white hocus:text-white": suGlobalMsgType === "info" || suGlobalMsgType === "error" || suGlobalMsgType === "success",
+                })
+              )}
+            >
               {suGlobalMsgLink.title}
             </ActionLink>
-          }
+          )}
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-const MessageIcon = ({
-  messageType,
-}: {
-  messageType: StanfordGlobalMessage["suGlobalMsgType"];
-}) => {
+const MessageIcon = ({messageType}: {messageType: StanfordGlobalMessage["suGlobalMsgType"]}) => {
   switch (messageType) {
     case "info":
-      return <InformationCircleIcon width={40} />;
+      return <InformationCircleIcon width={40} />
     case "success":
-      return <CheckCircleIcon width={40} />;
+      return <CheckCircleIcon width={40} />
     case "plain":
-      return <BellIcon width={40} />;
+      return <BellIcon width={40} />
   }
-  return <ExclamationTriangleIcon width={40} />;
-};
+  return <ExclamationTriangleIcon width={40} />
+}
 
-export default GlobalMessage;
+export default GlobalMessage
--- src/components/nodes/pages/summer-course/summer-course-page.tsx
+++ src/components/nodes/pages/summer-course/summer-course-page.tsx
@@ -1,20 +1,20 @@
-import Wysiwyg from "@components/elements/wysiwyg";
-import {H1, H3} from "@components/elements/headers";
-import {HtmlHTMLAttributes} from "react";
-import Image from "next/image";
-import {NodeSumSummerCourse} from "@lib/gql/__generated__/drupal.d";
-import ArchBanner from "@components/patterns/arch-banner";
-import { convertToLocalDateTime } from "@lib/utils/convert-date";
-import FavoritesList from "@components/elements/favorites-list";
+import Wysiwyg from "@components/elements/wysiwyg"
+import {H1, H2, H3} from "@components/elements/headers"
+import {HtmlHTMLAttributes} from "react"
+import Image from "next/image"
+import {NodeSumSummerCourse} from "@lib/gql/__generated__/drupal.d"
+import ArchBanner from "@components/patterns/arch-banner"
+import {convertToLocalDateTime} from "@lib/utils/convert-date"
+import FavoritesList from "@components/elements/favorites-list"
 
 type Props = HtmlHTMLAttributes<HTMLDivElement> & {
   node: NodeSumSummerCourse
   headingLevel?: "h2" | "h3"
 }
 
-const SummerCoursePage = ({ node, ...props }: Props) => {
-  const startDate = node.sumCourseStartDate && convertToLocalDateTime(node.sumCourseStartDate).toUpperCase();
-  const endDate = node.sumCourseEndDate && convertToLocalDateTime(node.sumCourseEndDate).toUpperCase();
+const SummerCoursePage = ({node, ...props}: Props) => {
+  const startDate = node.sumCourseStartDate && convertToLocalDateTime(node.sumCourseStartDate).toUpperCase()
+  const endDate = node.sumCourseEndDate && convertToLocalDateTime(node.sumCourseEndDate).toUpperCase()
 
   return (
     <article {...props}>
@@ -24,61 +24,79 @@ const SummerCoursePage = ({ node, ...props }: Props) => {
         imageAlt=""
       >
         <div className="w-screen">
-          <div className="flex flex-col justify-center items-center border-archway-dark border-b rs-pb-4 rs-mx-6">
-            {startDate &&
-              <div className="font-sans font-normal rs-mb-0">
-                {startDate}{endDate && (` — ${endDate}`)}
+          <div className="rs-pb-4 rs-mx-6 flex flex-col items-center justify-center border-b border-archway-dark">
+            {startDate && (
+              <div className="rs-mb-0 font-sans font-normal">
+                {startDate}
+                {endDate && ` — ${endDate}`}
               </div>
-            }
-            <H1 className="font-normal max-w-[900px] font-roboto text-center rs-mb-0">
-              {node.title}
-            </H1>
-            {node.sumCourseCatalogNumber &&
-              <div className="font-normal">{node.sumCourseCatalogNumber}</div>
-            }
+            )}
+            <H1 className="rs-mb-0 max-w-[900px] text-center font-roboto font-normal">{node.title}</H1>
+            {node.sumCourseCatalogNumber && <div className="font-normal">{node.sumCourseCatalogNumber}</div>}
           </div>
         </div>
       </ArchBanner>
-      <div className="centered my-32 grid grid-cols-12 gap-10 z-10 relative">
+      <div className="centered relative z-10 my-32 grid grid-cols-12 gap-10">
         <div className="order-2 col-span-12 lg:col-span-8">
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 rs-mb-4">
-            {node.sumCourseImage &&
-              <div className="relative aspect-w-1 aspect-h-1">
+          <div className="rs-mb-4 grid grid-cols-1 gap-10 lg:grid-cols-2">
+            {node.sumCourseImage && (
+              <div className="aspect-h-1 aspect-w-1 relative">
                 <Image
-                  className="object-cover rounded-[25px]"
+                  className="rounded-[25px] object-cover"
                   src={node.sumCourseImage.mediaImage.url}
                   alt={node.sumCourseImage.mediaImage.alt || ""}
                   fill
                   sizes="(max-width: 768px) 300px, 500px"
                 />
               </div>
-            }
+            )}
             <div className="pt-12 children:mb-5 [&_span]:font-bold">
-              <H3 className="text-m1 ">Details:</H3>
+              <H2 className="text-m1">Details:</H2>
 
-              {node.sumCourseSchedule && <div><span>Time:</span> {node.sumCourseSchedule}</div>} 
-              {node.sumCourseUnits && <div><span>Units:</span> {node.sumCourseUnits}</div>} 
-              
-              {node.sumCourseInterestArea && <div><span>Interest Area:</span> {node.sumCourseInterestArea.name}</div>} 
+              {node.sumCourseSchedule && (
+                <div>
+                  <span>Time:</span> {node.sumCourseSchedule}
+                </div>
+              )}
+              {node.sumCourseUnits && (
+                <div>
+                  <span>Units:</span> {node.sumCourseUnits}
+                </div>
+              )}
 
-              {node.sumCourseInstructors && 
+              {node.sumCourseInterestArea && (
+                <div>
+                  <span>Interest Area:</span> {node.sumCourseInterestArea.name}
+                </div>
+              )}
+
+              {node.sumCourseInstructors && (
                 <div>
                   <span>Instructor: </span>
-                      {node.sumCourseInstructors && node.sumCourseInstructors.length > 1 && node.sumCourseInstructors.join(", ")}
+                  {node.sumCourseInstructors && node.sumCourseInstructors.length > 1 && node.sumCourseInstructors.join(", ")}
                 </div>
-              }
+              )}
 
-              {node.sumCoursePopulation &&
+              {node.sumCoursePopulation && (
                 <div>
                   <span>Population: </span>
-                  {node.sumCoursePopulation.map((population) => population.name).join(", ")}
+                  {node.sumCoursePopulation.map(population => population.name).join(", ")}
                 </div>
-              }
-              
-              {node.sumCourseInterestArea && <div><span>Interest Area:</span> {node.sumCourseInterestArea.name}</div>}
+              )}
 
-              {node.sumCourseFormat && <div><span>Course Format & Length: </span>{node.sumCourseFormat}{node.sumCourseLength && `, ${node.sumCourseLength}`}</div>}
+              {node.sumCourseInterestArea && (
+                <div>
+                  <span>Interest Area:</span> {node.sumCourseInterestArea.name}
+                </div>
+              )}
 
+              {node.sumCourseFormat && (
+                <div>
+                  <span>Course Format & Length: </span>
+                  {node.sumCourseFormat}
+                  {node.sumCourseLength && `, ${node.sumCourseLength}`}
+                </div>
+              )}
 
               {node.sumCourseCrossListing && (
                 <div>
@@ -89,25 +107,27 @@ const SummerCoursePage = ({ node, ...props }: Props) => {
             </div>
           </div>
           <div>
-            {node.sumCourseDescription &&
-              <div className="rs-mb-1"><H3 className="text-m1 mb-5">Summary:</H3><Wysiwyg html={node.sumCourseDescription.processed} /></div>
-            }
-            {node.sumCourseNotes &&
-            <><H3 className="text-m1 mb-5">Course notes:</H3><Wysiwyg html={node.sumCourseNotes?.processed} /></>
-            }
+            {node.sumCourseDescription && (
+              <div className="rs-mb-1">
+                <H3 className="mb-5 text-m1">Summary:</H3>
+                <Wysiwyg html={node.sumCourseDescription.processed} />
+              </div>
+            )}
+            {node.sumCourseNotes && (
+              <>
+                <H3 className="mb-5 text-m1">Course notes:</H3>
+                <Wysiwyg html={node.sumCourseNotes?.processed} />
+              </>
+            )}
           </div>
         </div>
         <div className="order-1 col-span-12 lg:col-span-4">
           <FavoritesList isDisplayOnly />
         </div>
       </div>
-      <div>
-        {/* Related Courses Cards */}
-      </div>
-      <div>
-        {/* Apply Now Link */}
-      </div>
+      <div>{/* Related Courses Cards */}</div>
+      <div>{/* Apply Now Link */}</div>
     </article>
   )
 }
-export default SummerCoursePage;
+export default SummerCoursePage

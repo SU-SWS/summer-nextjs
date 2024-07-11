83a0be329c1f735f0ce44781675b06b1337c8036
rebeccahongsf <rebeccahongsf@gmail.com>
2024-07-11 07:54:44 -0700

    fixup formatting

--- src/components/algolia/algolia-course-related.tsx
+++ src/components/algolia/algolia-course-related.tsx
@@ -1,12 +1,12 @@
 "use client"
 
-import { useMemo } from "react"
+import {useMemo} from "react"
 import algoliasearch from "algoliasearch/lite"
 import {RelatedProducts} from "react-instantsearch"
 import {InstantSearchNext} from "react-instantsearch-nextjs"
-import ImageCard from '@components/patterns/image-card'
-import { H3 } from '@components/elements/headers'
-import ActionLink from '@components/elements/action-link'
+import ImageCard from "@components/patterns/image-card"
+import {H3} from "@components/elements/headers"
+import ActionLink from "@components/elements/action-link"
 
 type Props = {
   objectID: string
@@ -25,11 +25,22 @@ type RelatedCourseItemProps = {
 }
 
 const RelatedCourseItem = ({item}: {item: RelatedCourseItemProps}) => (
-  <ImageCard imageUrl={item.photo} imageAlt="" className="space-y-16 @container">
+  <ImageCard
+    imageUrl={item.photo}
+    imageAlt=""
+    className="space-y-16 @container"
+  >
     <div className="flex flex-col">
-      <H3 className="order-2 rs-mb-0 max-w-[900px] text-center font-roboto font-normal">{item.title}</H3>
+      <H3 className="rs-mb-0 order-2 max-w-[900px] text-center font-roboto font-normal">{item.title}</H3>
       <div className="order-1 font-normal">{item.sum_course_catalog_number}</div>
-      {item.url && <ActionLink className="order-3" href={item.url}>Learn more</ActionLink>}
+      {item.url && (
+        <ActionLink
+          className="order-3"
+          href={item.url}
+        >
+          Learn more
+        </ActionLink>
+      )}
     </div>
   </ImageCard>
 )
@@ -44,9 +55,12 @@ const RelatedCourses = ({objectID, appId, searchIndex, searchApiKey, relatedTopi
       future={{preserveSharedStateOnUnmount: true}}
     >
       <div className="gutters grid gap-10 @9xl:grid-cols-3 md:gap-20">
-        <ImageCard hasBgColor className="space-y-16 @container">
+        <ImageCard
+          hasBgColor
+          className="space-y-16 @container"
+        >
           <div className="flex flex-col">
-            <H3 className="order-2 rs-mb-0 max-w-[900px] text-center font-roboto font-normal">Explore more courses {relatedTopic}</H3>
+            <H3 className="rs-mb-0 order-2 max-w-[900px] text-center font-roboto font-normal">Explore more courses {relatedTopic}</H3>
             <div className="order-1 font-normal uppercase">Related Courses</div>
           </div>
         </ImageCard>

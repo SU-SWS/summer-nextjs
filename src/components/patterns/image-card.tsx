3f25b839900bfc06308face51a71bade47453066
rebeccahongsf <rebeccahongsf@gmail.com>
2024-07-11 16:37:42 -0700

    fixup

--- src/components/patterns/image-card.tsx
+++ src/components/patterns/image-card.tsx
@@ -34,7 +34,13 @@ const ImageCard = ({imageUrl, imageAlt, videoUrl, isArticle, children, hasBgColo
   return (
     <CardWrapper
       {...props}
-      className={twMerge("centered w-full rounded-[25px] lg:max-w-[920px] lg:max-w-[980px]", clsx({"bg-transparent": hasBgColor, "bg-fog-light": !hasBgColor, "lg:rs-pt-9": hasBgColor && !imageUrl}, props.className))}
+      className={twMerge(
+        "centered w-full rounded-[25px] lg:max-w-[920px] lg:max-w-[980px]",
+        clsx(
+          {"bg-transparent": hasBgColor, "bg-fog-light": !hasBgColor, "lg:rs-pt-9": hasBgColor && !imageUrl},
+          props.className
+        )
+      )}
     >
       {imageUrl && (
         <div className="relative aspect-[16/9] w-full">
@@ -50,7 +56,9 @@ const ImageCard = ({imageUrl, imageAlt, videoUrl, isArticle, children, hasBgColo
 
       {videoUrl && <Oembed url={videoUrl} />}
 
-      <div className={clsx("flex flex-col", {"px-10 py-20 lg:px-20": !hasBgColor, "pt-10": hasBgColor && imageUrl})}>{children}</div>
+      <div className={clsx("flex flex-col", {"px-10 py-20 lg:px-20": !hasBgColor, "pt-10": hasBgColor && imageUrl})}>
+        {children}
+      </div>
     </CardWrapper>
   )
 }

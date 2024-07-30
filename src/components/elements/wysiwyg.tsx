74664181f7f57f0fad9faaa84318a5e51e3bb505
rebeccahongsf <rebeccahongsf@gmail.com>
2024-07-30 14:01:09 -0700

    fixup border; remove extra left-vertical

--- src/components/elements/wysiwyg.tsx
+++ src/components/elements/wysiwyg.tsx
@@ -63,7 +63,7 @@ const options: HTMLReactParserOptions = {
           if (nodeProps.className && nodeProps.className.includes("left-bar")) {
             nodeProps.className = twMerge(
               nodeProps.className,
-              "max-w-[725px] rs-pb-5 rs-pt-7 rs-pl-1 border-3 border-archway-dark border-opacity-50"
+              "max-w-[725px] rs-pb-5 rs-pt-7 rs-pl-1 border-l-3 border-archway-dark border-opacity-50"
             )
             return <NodeName {...nodeProps}>{domToReact(children, options)}</NodeName>
           }
@@ -130,12 +130,6 @@ const options: HTMLReactParserOptions = {
         case "tfoot":
         case "caption":
           return <NodeName {...nodeProps}>{domToReact(children, options)}</NodeName>
-        case "left-vertical":
-          return (
-            <div {...nodeProps} className="">
-              {domToReact(children, options)}
-            </div>
-          )
 
         // Void element tags like <br>, <hr>, <source>, etc.
         // @see https://developer.mozilla.org/en-US/docs/Glossary/Void_element

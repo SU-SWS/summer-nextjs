4305482af489c6fb86178a4dc0a78e6f36e3696c
rebeccahongsf <rebeccahongsf@gmail.com>
2024-08-28 15:52:46 -0700

    add bg transparent for larger screens

--- src/components/paragraphs/sum-arc-banner/sum-arc-banner-paragraph.tsx
+++ src/components/paragraphs/sum-arc-banner/sum-arc-banner-paragraph.tsx
@@ -14,12 +14,13 @@ const SumArcBannerParagraph = async ({paragraph, pageTitle, ...props}: Props) =>
     <div {...props}>
       <ArcBanner imageUrl={paragraph.sumArcImage?.mediaImage.url} imageAlt={paragraph.sumArcImage?.mediaImage.alt}>
         <div className="w-screen pb-32">
-          <div className="rs-pb-4 rs-mx-6 flex flex-col items-center justify-center border-b border-archway-dark">
+          <div className="rs-mx-6 flex flex-col items-center justify-center">
             {paragraph.sumArcSuperhead && (
               <div className="rs-mb-0 font-sans font-normal">{paragraph.sumArcSuperhead}</div>
             )}
             <H1 className="rs-mb-0 max-w-[900px] text-center font-roboto font-normal">{pageTitle}</H1>
             <Wysiwyg html={paragraph.sumArcDescription?.processed} className="mx-auto max-w-[450px] text-center" />
+            <div className="rs-pb-4 w-full max-w-[1200px] border-b border-archway-dark" />
           </div>
         </div>
       </ArcBanner>

2163d34434e45f4a22e99d7aa4cb8e20ec346b11
rebeccahongsf <rebeccahongsf@gmail.com>
2024-08-27 21:07:10 -0700

    add border top option and adjust max height for wrapper

--- src/components/patterns/arc-banner.tsx
+++ src/components/patterns/arc-banner.tsx
@@ -29,10 +29,7 @@ const ArcBanner = ({imageUrl, imageAlt, isSection, isBorder, children, ...props}
   return (
     <BannerWrapper
       {...props}
-      className={twMerge(
-        "relative z-0 h-full overflow-hidden @container md:max-h-[875px] md:min-h-[400px] lg:max-h-[840px]",
-        props.className
-      )}
+      className={twMerge("relative z-0 h-full max-h-fit overflow-hidden @container md:min-h-[400px]", props.className)}
     >
       <div className="@6xl:aspect-auto absolute z-10 aspect-[16/9] w-full bg-cool-grey @6xl:absolute @6xl:h-full">
         {imageUrl && (
@@ -47,7 +44,12 @@ const ArcBanner = ({imageUrl, imageAlt, isSection, isBorder, children, ...props}
         )}
       </div>
       <div className="h-1/2">
-        <div className="rs-mt-8 absolute left-[-10%] z-20 box-border aspect-[2/1] min-h-[400px] w-[120%] rounded-t-full border-t border-archway-light bg-white" />
+        <div
+          className={clsx(
+            "rs-mt-8 absolute left-[-10%] z-20 box-border aspect-[2/1] min-h-[400px] w-[120%] rounded-t-full border-t border-archway-light bg-white",
+            {"border-t-2": isBorder}
+          )}
+        />
       </div>
       {children && (
         <div className="relative z-50 mt-[150px] h-full rounded-t-full bg-white md:mt-[250px] xl:mt-[350px] 2xl:mt-[400px]">

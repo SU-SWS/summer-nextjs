1d46ad15ba54d2974f3f6b9788d433e2a5c3be79
rebeccahongsf <rebeccahongsf@gmail.com>
2024-09-05 14:44:28 -0700

    SUM-251 | adjust button size on testimonial banner

--- src/components/paragraphs/sum-testimonial-banner/sum-testimonial-banner-paragraph.tsx
+++ src/components/paragraphs/sum-testimonial-banner/sum-testimonial-banner-paragraph.tsx
@@ -156,7 +156,7 @@ const SumTestimonialBannerParagraph = ({paragraph, ...props}: Props) => {
 
           {paragraph.sumTestimonialButton?.url && (
             <ActionLink
-              className="btn rs-mt-5 block w-fit rounded-full bg-digital-red px-12 py-6 text-5xl font-normal text-white no-underline transition hocus:text-white hocus:underline"
+              className="btn rs-mt-5 flex w-fit items-center rounded-full bg-digital-red px-8 py-4 font-normal text-white no-underline transition hocus:text-white hocus:underline"
               href={paragraph.sumTestimonialButton.url}
             >
               {paragraph.sumTestimonialButton.title}

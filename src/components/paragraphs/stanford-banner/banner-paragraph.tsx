3af4e7823c4cd1faa8aebf44229570549e866ed6
rebeccahongsf <rebeccahongsf@gmail.com>
2024-09-05 17:12:41 -0700

    button cleanup

--- src/components/paragraphs/stanford-banner/banner-paragraph.tsx
+++ src/components/paragraphs/stanford-banner/banner-paragraph.tsx
@@ -68,7 +68,7 @@ const BannerParagraph = ({paragraph, eagerLoadImage, ...props}: Props) => {
           {paragraph.suBannerButton?.url && !semiCircleButton && (
             <div className="rs-pt-2">
               <ActionLink
-                className="btn flex w-fit items-center rounded-full border-2 border-transparent bg-digital-red px-8 py-4 font-normal text-white no-underline transition hocus:border-white hocus:text-white hocus:underline"
+                className="btn w-fit rounded-full border-2 border-transparent bg-digital-red px-8 py-4 font-normal text-white transition hocus:border-white hocus:text-white"
                 href={paragraph.suBannerButton.url}
               >
                 {paragraph.suBannerButton.title}

f9b6fae15fb17e3408ad410286b931774fc057a3
rebeccahongsf <rebeccahongsf@gmail.com>
2024-09-12 14:26:03 -0700

    SUM-257 | rework arc banner

--- src/components/patterns/arc-banner.tsx
+++ src/components/patterns/arc-banner.tsx
@@ -29,9 +29,9 @@ const ArcBanner = ({imageUrl, imageAlt, isSection, isBorder, children, ...props}
   return (
     <BannerWrapper
       {...props}
-      className={twMerge("relative z-0 h-full max-h-fit overflow-hidden @container md:min-h-[400px]", props.className)}
+      className={twMerge("relative h-full max-h-fit overflow-hidden @container md:min-h-[400px]", props.className)}
     >
-      <div className="@6xl:aspect-auto absolute z-10 aspect-[16/9] w-full bg-white @6xl:absolute @6xl:h-full">
+      <div className="4xl:aspect-auto absolute -z-50 aspect-[16/9] w-full bg-white 4xl:h-full">
         {imageUrl && (
           <Image
             className="ed11y-ignore object-cover"
@@ -42,33 +42,18 @@ const ArcBanner = ({imageUrl, imageAlt, isSection, isBorder, children, ...props}
             sizes="100vw"
           />
         )}
-      </div>
-      <div
-        className={twMerge(
-          "clip-arc absolute z-20 mt-10 hidden h-full w-full border-black bg-white md:mt-28 4xl:block",
-          clsx({
-            "before:clip-arc overflow-hidden bg-black-true before:absolute before:left-[2px] before:top-[2px] before:z-[1] before:h-[calc(100%-2px)] before:w-[calc(100%-4px)] before:bg-white before:content-['']":
-              isBorder,
-          })
-        )}
-      />
-      <div className="block h-1/2 4xl:hidden">
         <div
-          className={clsx(
-            "rs-mt-8 absolute left-[-10%] z-20 box-border aspect-[2/1] h-fit min-h-[400px] w-[120%] rounded-t-full border-t border-archway-light bg-white",
-            {"border-t-2": isBorder}
+          className={twMerge(
+            "clip-arc relative h-full w-full border-black bg-white",
+            clsx({
+              "before:clip-arc overflow-hidden bg-black-true before:absolute before:left-[2px] before:top-[2px] before:z-[1] before:h-[calc(100%-2px)] before:w-[calc(100%-4px)] before:bg-white before:content-['']":
+                isBorder,
+            })
           )}
         />
       </div>
       {children && (
-        <div
-          className={clsx("relative z-50 mt-[150px] h-full rounded-t-full md:mt-[250px] xl:mt-[350px] 2xl:mt-[400px]", {
-            "bg-white 2xl:bg-transparent": !isBorder,
-            "bg-transparent": isBorder,
-          })}
-        >
-          {children}
-        </div>
+        <div className="z-30 items-center pt-[150px] md:pt-[250px] xl:pt-[350px] 2xl:pt-[400px]">{children}</div>
       )}
     </BannerWrapper>
   )

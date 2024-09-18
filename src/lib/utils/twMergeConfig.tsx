94c6ae8809bfecd9b9d433d92ddda60673de49df
rebeccahongsf <rebeccahongsf@gmail.com>
2024-09-18 12:19:08 -0700

    add text-# options and export as twMerge

--- src/lib/utils/twMergeConfig.tsx
+++ src/lib/utils/twMergeConfig.tsx
@@ -1,12 +1,18 @@
 import {extendTailwindMerge} from "tailwind-merge"
 
-const customTwMerge = extendTailwindMerge({
+// Creates an array containing type-# classes from 0 to 1
+const typeClasses = Array.from({length: 10}, (_, i) => `type-${i}`)
+// Creates an array containing text-# classes from 11 to 30
+const textClasses = Array.from({length: 20}, (_, i) => `text-${i + 11}`)
+
+const typographyClasses = [...textClasses, ...typeClasses]
+
+const twMerge = extendTailwindMerge({
   extend: {
     classGroups: {
-      // Creates an array containing type-# classes from 0 to 1
-      "font-size": Array.from({length: 10}, (_, i) => `type-${i}`),
+      "font-size": typographyClasses,
     },
   },
 })
 
-export default customTwMerge
+export default twMerge

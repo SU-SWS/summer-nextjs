69ce09bd8d48cd4d61964eb7243b93c693711407
Jen Breese-Kauth <jbreese@stanford.edu>
2024-12-20 19:08:55 +0000

    removed test text

--- src/components/elements/toast-message.tsx
+++ src/components/elements/toast-message.tsx
@@ -4,21 +4,18 @@ import {toast, ToastContainer} from "react-toastify"
 
 export function ToastMessage() {
   return (
-    <>
-      <p>hi</p>
-      <ToastContainer
-        progressClassName="bg-spirited-light"
-        position="bottom-left"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick={false}
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
-    </>
+    <ToastContainer
+      progressClassName="bg-spirited-light"
+      position="bottom-left"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="dark"
+    />
   )
 }

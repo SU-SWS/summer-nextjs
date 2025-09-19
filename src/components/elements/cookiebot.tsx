5ecfb3f020f6eff41f83c14e3463de7e742ccdf1
Mike Decker <pookmish@stanford.edu>
2025-09-19 13:37:00 -0700

    reduce number of renders

--- src/components/elements/cookiebot.tsx
+++ src/components/elements/cookiebot.tsx
@@ -1,7 +1,8 @@
 "use client"
 
 import ReactCookieBot from "react-cookiebot"
-import {useEventListener} from "usehooks-ts"
+import {useBoolean, useEventListener, useIsClient} from "usehooks-ts"
+import {useEffect, useLayoutEffect} from "react"
 
 const Cookiebot = () => {
   const cookieBotConsent = () => {
@@ -11,12 +12,20 @@ const Cookiebot = () => {
     }
   }
 
+  const {value: onlyBot, setFalse} = useBoolean(true)
+  const isClient = useIsClient()
+  useLayoutEffect(() => cookieBotConsent(), [])
+  useEffect(() => {
+    if (document.getElementById("CookieBot")) setFalse()
+  }, [setFalse])
+
   // Cookiebot event listeners. See https://www.cookiebot.com/en/developer/#h-event-handling
   // @ts-expect-error Cookiebot window event is custom.
   useEventListener("CookiebotOnAccept", cookieBotConsent)
   // @ts-expect-error Cookiebot window event is custom.
   useEventListener("CookiebotOnDecline", cookieBotConsent)
-  return <ReactCookieBot domainGroupId="bcda1e2f-63c7-4cad-81b4-04a49e30b05f" />
+
+  if (isClient && onlyBot) return <ReactCookieBot domainGroupId="bcda1e2f-63c7-4cad-81b4-04a49e30b05f" />
 }
 
 export default Cookiebot

6e3f4663393f42b5d6a753276fb637d3d3c71db5
Mike Decker <pookmish@stanford.edu>
2025-09-19 13:23:03 -0700

    Cleanup ts

--- src/components/elements/cookiebot.tsx
+++ src/components/elements/cookiebot.tsx
@@ -7,14 +7,14 @@ const Cookiebot = () => {
   const cookieBotConsent = () => {
     const consent = window.Cookiebot?.consent?.statistics ? "granted" : "denied"
     if (typeof window.clarity !== "undefined") {
-      console.warn("clarity consent: " + consent)
       window.clarity("consentv2", {ad_Storage: consent, analytics_Storage: consent})
     }
   }
 
-  // @ts-ignore
+  // Cookiebot event listeners. See https://www.cookiebot.com/en/developer/#h-event-handling
+  // @ts-expect-error Cookiebot window event is custom.
   useEventListener("CookiebotOnAccept", cookieBotConsent)
-  // @ts-ignore
+  // @ts-expect-error Cookiebot window event is custom.
   useEventListener("CookiebotOnDecline", cookieBotConsent)
   return <ReactCookieBot domainGroupId="bcda1e2f-63c7-4cad-81b4-04a49e30b05f" />
 }

c072e0af25f95cfe7703d2fb274ebf2e3feae51e
Mike Decker <pookmish@stanford.edu>
2026-04-21 12:46:43 -0700

    Revert "Pull credentials from vault"
    This reverts commit a3d4ca872a2fa8f48ed6350b67562f3b701d7eab.

--- src/components/elements/cookiebot.tsx
+++ src/components/elements/cookiebot.tsx
@@ -1,8 +1,8 @@
 "use client"
 
 import ReactCookieBot from "react-cookiebot"
-import {useEventListener, useIsClient} from "usehooks-ts"
-import {useLayoutEffect} from "react"
+import {useBoolean, useEventListener, useIsClient} from "usehooks-ts"
+import {useEffect, useLayoutEffect} from "react"
 
 const Cookiebot = () => {
   const cookieBotConsent = () => {
@@ -12,8 +12,12 @@ const Cookiebot = () => {
     }
   }
 
+  const {value: onlyBot, setFalse} = useBoolean(true)
   const isClient = useIsClient()
   useLayoutEffect(() => cookieBotConsent(), [])
+  useEffect(() => {
+    if (document.getElementById("CookieBot")) setFalse()
+  }, [setFalse])
 
   // Cookiebot event listeners. See https://www.cookiebot.com/en/developer/#h-event-handling
   // @ts-expect-error Cookiebot window event is custom.
@@ -21,8 +25,7 @@ const Cookiebot = () => {
   // @ts-expect-error Cookiebot window event is custom.
   useEventListener("CookiebotOnDecline", cookieBotConsent)
 
-  if (isClient && !document.getElementById("CookieBot"))
-    return <ReactCookieBot domainGroupId="bcda1e2f-63c7-4cad-81b4-04a49e30b05f" />
+  if (isClient && onlyBot) return <ReactCookieBot domainGroupId="bcda1e2f-63c7-4cad-81b4-04a49e30b05f" />
 }
 
 export default Cookiebot
